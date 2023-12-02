e2b707ba4fa97204653d426d6eb5d53847ba2ea9
giaanbiot <151908352+giaanbiot@users.noreply.github.com>
2023-12-02 12:50:58 +0800

    Add files via upload

---
+++ paragraphs.js
@@ -0,0 +1,7 @@
+const paragraphs = [
+    "As Kunkka watched the demons take his ships down one by one, he had the satisfaction of wearing away their fleet with his ancestral magic. Lord of Heaven, father of gods, Zeus treats all the Heroes as if they are his rambunctious, rebellious children.",
+    "At some point in time, Rubick captured a mysterious puzzle named the Puzzle of Perplex in a special staff. He has spent his idle moments attempting to unlock this mysterious puzzle, but he could not solve it by himself.",
+    "Among the emblematic powers that populate the Underscape, Razor the Lightning Revenant is one of the most feared. With his whip of lightning, he patrols the Narrow Maze, that treacherous webwork of passages by which the souls of the dead are sorted according to their own innate intelligence, cunning and persistence.",
+    "The Tidehunter known as Leviathan was once the champion of the Sunken Isles, but his motives are as mysterious as those of his people. We all know the importance of the Drylanders' shipping lanes, how empires may rise and fall according to who controls the open water.",
+    "As Azwraith drove his pike through Vorn, the Dread Magus shattered into innumerable shards of light, saturating his killer with a strange new power. Once a humble fisherman, the Phantom Lancer Spears targets from afar, spawning a fragment of the Phantom Lancer himself as they Dash ahead while cutting foes apart in a breathtaking flurry of attacks. ",
+];
